3c2fff1bda9f7d333de1b2fb995c4673bc73bed9
david-gimeno <dagigo1@dsic.upv.es>
2022-03-28 12:54:32 +0200

    Modifying MAIN.sh

--- MAIN.sh
+++ MAIN.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+rtve2018_path=$1
 ## Unzipping data ##
 unzip ./src/transcriptions.zip -d ./data/transcriptions/
 unzip ./src/landmarks.zip -d ./data/landmarks/
 
 ## Obtaining LIP-RTVE samples from source data alignmets ##
-python3 src/scripts/get_samples.py --alignments ./src/alignments.csv --source-dir ./data/RTVE2018-20H/ --output-dir ./data/LIP-RTVE/
+python3 src/scripts/get_samples.py --alignments ./src/alignments.csv --source-dir $rtve2018_path --output-dir ./data/LIP-RTVE/
 
 ## Extracting LIP-RTVE's Regions of Interest ##
 python3 src/scripts/get_ROIs.py --samples-dir ./data/LIP-RTVE/ --landmarks-dir ./data/landmarks/ --roi-types fitMouths wideMouths faces SOTA --output-dir ./data/ROIs/
