d874996ed288a6073cfbec1267743ab1b8eb3f08
david-gimeno <dagigo1@dsic.upv.es>
2022-05-18 10:08:56 +0200

    Modifying main.sh

--- main.sh
+++ main.sh
@@ -6,17 +6,28 @@ mkdir ./data/
 mkdir ./data/transcriptions/
 mkdir ./data/landmarks/
 
-echo "Downloading landmarks.zip ..."
+echo -e "#############################"
+echo -e "Downloading landmarks.zip ..."
+echo -e "#############################"
 gdown -O ./src/ https://drive.google.com/uc?id=13oIvMv-xHp9t24RCCNOeIA6YvsAoZw9K
 
-echo "Unzipping data"
+echo -e "##############"
+echo -e "Unzipping data"
+echo -e "##############"
+
 unzip ./src/transcriptions.zip -d ./data/transcriptions/
 unzip ./src/landmarks.zip -d ./data/landmarks/
 
-echo "Obtaining LIP-RTVE samples from source data alignments"
+echo -e "######################################################"
+echo -e "Obtaining LIP-RTVE samples from source data alignments"
+echo -e "######################################################"
 python3 src/scripts/get_samples.py --alignments ./src/alignments.csv --source-dir $RTVE2018_path --output-dir ./data/LIP-RTVE/
 
-echo "Extracting LIP-RTVE's Regions of Interest"
+echo -e "#########################################"
+echo -e "Extracting LIP-RTVE's Regions of Interest"
+echo -e "#########################################"
 python3 src/scripts/get_ROIs.py --samples-dir ./data/LIP-RTVE/mp4/ --landmarks-dir ./data/landmarks/ --roi-types fitMouths wideMouths faces SOTA --output-dir ./data/ROIs/
 
-echo "\nLIP-RTVE completed!"
+echo -e "###################"
+echo -e "LIP-RTVE completed!"
+echo -e "###################"
